f7dd57ce130cd8827be7d079e699b73c998a73b0
Chris Puska <theta@hey.com>
2024-02-11 09:02:44 -0800

    Tabs

--- packages/workshop/app/components/ExampleTabs/ExampleTabs.tsx
+++ packages/workshop/app/components/ExampleTabs/ExampleTabs.tsx
@@ -1,11 +1,25 @@
 "use client"
 
 import type { FC } from "react"
-import { Tabs } from "@heliosgraphics/ui"
+import { Flex, Tabs } from "@heliosgraphics/ui"
 import type { ExampleTabsProps } from "./ExampleTabs.types"
 
 const ExampleTabs: FC<ExampleTabsProps> = () => {
-	return <Tabs items={[]} sections={[]} />
+	const tabItems = ["First", "Second", "Third"]
+
+	const tabSections = [
+		<Flex key={0} padding={8}>
+			1. Content
+		</Flex>,
+		<Flex key={1} padding={8}>
+			2. Content
+		</Flex>,
+		<Flex key={1} padding={8}>
+			3. Content
+		</Flex>,
+	]
+
+	return <Tabs items={tabItems} sections={tabSections} />
 }
 
 export default ExampleTabs
