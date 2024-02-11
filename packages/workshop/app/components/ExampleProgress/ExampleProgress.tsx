3c3e5686f87a0726e7e8fd69160f193edaaf0d31
Chris Puska <theta@hey.com>
2024-02-11 08:39:59 -0800

    Progress example

--- packages/workshop/app/components/ExampleProgress/ExampleProgress.tsx
+++ packages/workshop/app/components/ExampleProgress/ExampleProgress.tsx
@@ -1,11 +1,17 @@
 "use client"
 
-import { Progress } from "@heliosgraphics/ui"
+import { Flex, Progress } from "@heliosgraphics/ui"
 import type { FC } from "react"
 import type { ExampleProgressProps } from "./ExampleProgress.types"
 
 const ExampleProgress: FC<ExampleProgressProps> = () => {
-	return <Progress max={100} value={24} />
+	return (
+		<Flex isColumn={true} gap={8}>
+			<Progress max={100} value={12} />
+			<Progress max={100} value={68} />
+			<Progress max={100} value={92} />
+		</Flex>
+	)
 }
 
 export default ExampleProgress
