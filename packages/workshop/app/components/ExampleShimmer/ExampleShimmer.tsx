2176f7169259cdbe84a26257eb83e07d20f137b7
Chris Puska <theta@hey.com>
2024-02-11 08:43:07 -0800

    Shimmer example

--- packages/workshop/app/components/ExampleShimmer/ExampleShimmer.tsx
+++ packages/workshop/app/components/ExampleShimmer/ExampleShimmer.tsx
@@ -1,11 +1,16 @@
 "use client"
 
-import { Shimmer } from "@heliosgraphics/ui"
+import { Flex, Shimmer } from "@heliosgraphics/ui"
 import type { FC } from "react"
 import type { ExampleShimmerProps } from "./ExampleShimmer.types"
 
 const ExampleShimmer: FC<ExampleShimmerProps> = () => {
-	return <Shimmer height={20} width={120} />
+	return (
+		<Flex isColumn={true} gap={8}>
+			<Shimmer height={20} width={120} />
+			<Shimmer height={20} width={120} />
+		</Flex>
+	)
 }
 
 export default ExampleShimmer
