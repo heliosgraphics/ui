63202f53aad832fd8bbf1ded1d4fae226c12dfa1
Chris Puska <theta@hey.com>
2024-02-11 08:59:13 -0800

    Timestamp example

--- packages/workshop/app/components/ExampleTimestamp/ExampleTimestamp.tsx
+++ packages/workshop/app/components/ExampleTimestamp/ExampleTimestamp.tsx
@@ -1,11 +1,27 @@
 "use client"
 
-import type { FC } from "react"
-import { Timestamp } from "@heliosgraphics/ui"
+import { useState, type FC } from "react"
+import { Button, ButtonGroup, Flex, Separator, Timestamp } from "@heliosgraphics/ui"
 import type { ExampleTimestampProps } from "./ExampleTimestamp.types"
 
 const ExampleTimestamp: FC<ExampleTimestampProps> = () => {
-	return <Timestamp />
+	const [isFromNow, setFromNow] = useState<boolean>(false)
+
+	const onFromNowClick = () => setFromNow(!isFromNow)
+
+	const date: string = new Date().toISOString()
+
+	return (
+		<Flex isColumn={true} gap={8}>
+			<ButtonGroup>
+				<Button intent="silent" size="small" value="From Now" onClick={onFromNowClick} />
+			</ButtonGroup>
+			<Separator isLight={true} />
+			<Timestamp date={date} fromNow={isFromNow} />
+			<Timestamp date={date} text="Date: " fromNow={isFromNow} />
+			<Timestamp date={date} text="Format: " format="YYYY-MM-DD" fromNow={isFromNow} />
+		</Flex>
+	)
 }
 
 export default ExampleTimestamp
