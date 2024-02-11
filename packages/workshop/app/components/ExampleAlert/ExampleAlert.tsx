10de274fd74bebac4d4dda278dd98a5c2811676c
Chris Puska <theta@hey.com>
2024-02-11 14:03:56 -0800

    Merge ui with gray shade

--- packages/workshop/app/components/ExampleAlert/ExampleAlert.tsx
+++ packages/workshop/app/components/ExampleAlert/ExampleAlert.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useContext } from "react"
-import { Flex, Alert, HeliosIntentionType } from "@heliosgraphics/ui"
+import { Button, Flex, Alert, HeliosIntentionType, ButtonGroup } from "@heliosgraphics/ui"
 import { WorkshopContext } from "../../contexts/WorkshopContext"
 import type { FC } from "react"
 import type { ExampleAlertProps } from "./ExampleAlert.types"
@@ -23,10 +23,13 @@ const ExampleAlert: FC<ExampleAlertProps> = () => {
 	const onClose = () => console.log(0)
 
 	return (
-		<Flex isColumn={true} gap={12}>
+		<Flex isColumn={true} gap={0}>
 			<Alert intent={intent} icon="info" onClose={onClose}>
 				{INTENT_DESCRIPTIONS[intent]}
 			</Alert>
+			<ButtonGroup>
+				<Button value="Tes" intent="silent" isDisabled={true} />
+			</ButtonGroup>
 			<Alert title="Vitae, assumenda aliquid" intent={intent} icon="info" onClose={onClose}>
 				Cupiditate <a>aspernatur autem</a> tempore magnam vitae, assumenda aliquid ab illo necessitatibus velit
 				temporibus corrupti quidem. Incidunt facilis aspernatur corrupti doloremque?
