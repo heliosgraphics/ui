42c63d5417d4a2f6555901a1c1e63c206c51a8b3
Chris Puska <theta@hey.com>
2024-02-22 15:17:02 -0800

    Accept ReactElement as array

--- packages/components/types/components.ts
+++ packages/components/types/components.ts
@@ -1,4 +1,4 @@
 import type { ReactElement } from 'react'
 
 export type HeliosOnClickType = (_?: unknown) => Promise<unknown> | void
-export type HeliosChildrenType = ReactElement<unknown> | null
+export type HeliosChildrenType = Array<ReactElement<unknown>> | ReactElement<unknown> | null
