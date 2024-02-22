990803100ad26454f88179237146d7526cba9119
Chris Puska <theta@hey.com>
2024-02-21 19:25:31 -0800

    Directly linked Confirm types

--- packages/components/components/Confirm/Confirm.tsx
+++ packages/components/components/Confirm/Confirm.tsx
@@ -17,21 +17,13 @@ const Confirm: FC<ConfirmProps> = ({
 	cancelText,
 	confirmText,
 }) => {
-	const onCancelClick = (event) => onCancel(event)
-	const onConfirmClick = (event): void => {
-		onCancel(event)
-		onConfirm(event)
-
-		return void 0
-	}
-
 	return (
 		<Dialog title={title} onClose={onCancel} isOpen={isOpen} isCentered={true}>
 			<Flex gap={12} isColumn={true}>
 				{!!description && <Text type="paragraph">{description}</Text>}
 				<ButtonGroup>
-					<Button intent={intent} value={confirmText} onClick={onConfirmClick} icon={icon} />
-					<Button intent="silent" value={cancelText} onClick={onCancelClick} />
+					<Button intent={intent} value={confirmText} onClick={onConfirm} icon={icon} />
+					<Button intent="silent" value={cancelText} onClick={onCancel} />
 				</ButtonGroup>
 			</Flex>
 		</Dialog>
