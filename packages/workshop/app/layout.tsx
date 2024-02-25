3028e05c13d1b488ba7864fd6d1246399a678e66
Chris Puska <theta@hey.com>
2024-02-25 10:14:41 -0800

    Meta: Add favicon

--- packages/workshop/app/layout.tsx
+++ packages/workshop/app/layout.tsx
@@ -6,7 +6,15 @@ import type { ReactNode } from "react"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
-	title: "Helios UI Workshop",
+	title: "Helios UI",
+	description: "Helios visual language and abstraction system",
+	icons: {
+		icon: "/favicon.svg",
+	},
+	metadataBase: new URL("https://ui.helios.graphics"),
+	openGraph: {
+		images: "/1200x630-ui-image.png",
+	},
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
