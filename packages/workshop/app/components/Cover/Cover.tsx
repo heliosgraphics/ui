261b85525d84ff5e0e49c0e04b048a463ab41ceb
Chris Puska <theta@hey.com>
2024-03-09 09:59:14 -0800

    Smaller cover on components page

--- packages/workshop/app/components/Cover/Cover.tsx
+++ packages/workshop/app/components/Cover/Cover.tsx
@@ -5,20 +5,23 @@ import { getClasses } from "@heliosgraphics/utils"
 import { useContext, type FC } from "react"
 import { WorkshopContext } from "workshop/app/contexts/WorkshopContext"
 import type { CoverProps } from "./Cover.types"
+import { usePathname } from "next/navigation"
 
 const NUMBER = 96 as const
 
 const Cover: FC<CoverProps> = () => {
 	const { hasCover } = useContext(WorkshopContext)
+	const pathname = usePathname()
 
 	if (!hasCover) return null
 
+	const isComponentPage: boolean = pathname.includes("components")
+	const coverClasses: string = getClasses(styles.cover, { [styles.coverAlt]: isComponentPage })
+
 	return (
-		<div className={styles.cover}>
+		<div className={coverClasses}>
 			{Array.from({ length: NUMBER }, (_, key) => {
-				const randomNumber = Math.floor(Math.random() * 12) + 1
-				const isRandom: boolean = key % randomNumber === 0
-				const tileClasses: string = getClasses(styles.coverTile, { [styles.coverTileAlt]: isRandom })
+				const tileClasses: string = getClasses(styles.coverTile)
 
 				return <div key={key} className={tileClasses}></div>
 			})}
